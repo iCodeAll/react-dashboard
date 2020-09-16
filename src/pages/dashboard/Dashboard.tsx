b045d45c41a4b92e7a64d1e21c558aeae37149d3
Jomarie Janer <jomariejaner258@gmail.com>
2020-09-17 03:46:24 +0800

    v1.0.0

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import './Style.scss';
+const Dashboard: React.FC = () => {
+  return <div className="page-wrapper"></div>;
+};
+export default Dashboard;
---
+++ src/routing.ts
@@ -0,0 +1,9 @@
+import  Dashboard from './pages/dashboard/Dashboard'
+
+export const urls = {
+  root: '/'
+}
+
+export const routes = [
+  { url: urls.root, content: Dashboard },
+]
